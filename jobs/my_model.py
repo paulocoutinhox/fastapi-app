b917f0d2269252b3bb9496c6f1de869d24d8d2b7
Paulo Coutinho <paulo@ubook.com>
2025-08-15 18:42:45 -0300

    more tests, better sqlalchemy2 and pydantic2 code, some refactor

--- jobs/my_model.py
+++ jobs/my_model.py
@@ -1,3 +1,4 @@
+from helpers.db import AsyncSessionLocal
 from helpers.scheduler import scheduler
 from models.my_model import MyModel
 from services import my_model as service_my_model
@@ -5,8 +6,6 @@ from services import my_model as service_my_model
 
 @scheduler.scheduled_job("cron", hour=0, minute=1)
 async def job_create_my_model():
-    # Import here to avoid circular imports and allow proper testing
-    from helpers.db import AsyncSessionLocal
 
     async with AsyncSessionLocal() as session:
         data = {"field1": "Test Job", "field2": False}
